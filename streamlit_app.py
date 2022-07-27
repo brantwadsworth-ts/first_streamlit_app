cfaca1714b95427a86d9e8b013380031852b2de3
Brant Wadsworth <108020695+brantwadsworth-ts@users.noreply.github.com>
2022-07-27 09:40:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega # & Blueberry Oatmeal')
-streamlit.text('🥗 , Simach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
