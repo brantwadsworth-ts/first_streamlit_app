be3dc6ba64f42aa68ca6bdd64e34f4ef61464d9b
Brant Wadsworth <108020695+brantwadsworth-ts@users.noreply.github.com>
2022-07-27 09:36:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega # & Blueberry Oatmeal')
+streamlit.text('Kale, Simach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
